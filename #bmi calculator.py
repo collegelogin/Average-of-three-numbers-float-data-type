167e61f8fdc31514bfd8dcd56bb26ae59af7c03b
collegelogin <collegelogin000@gmail.com>
2026-08-03 11:25:46 +1200

    simple bmi calculator
    height and weight are input by the user and bmi is calculatee

---
+++ #bmi calculator.py
@@ -0,0 +1,5 @@
+#bmi calculator
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
+bmi = weight / (height ** 2)
+print(f"Your BMI is: {bmi:.2f}")
