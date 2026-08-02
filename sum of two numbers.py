2fac29cde7a867743b1615d3ce0c6d2c50dd3604
collegelogin <collegelogin000@gmail.com>
2026-08-02 21:30:51 +1200

    Three test numbers are calculated and average is calculated.
    First user enters the test number and average is calculated. The obtained result will be float/int because the input data are in a float.

---
+++ sum of two numbers.py
@@ -0,0 +1,5 @@
+test1 =float( input("Enter first test number: "))
+test2 = float(input("Enter second test number: "))
+test3 = float(input("Enter third test number: "))
+Average = (test1 +test2+test3)/3
+print("The average of three test numbers is: ", Average)
