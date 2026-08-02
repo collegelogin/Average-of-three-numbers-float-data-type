0bea28186485b4405c43d4363da1d881cb50ac0a
collegelogin <collegelogin000@gmail.com>
2026-08-02 21:40:23 +1200

    sum and product of two number
    here the number are requested by user and data type float is used

---
+++ sum and product of two number.py
@@ -0,0 +1,7 @@
+#ask the user to enter two numbers and calculate sum and product
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+sum = a + b
+product = a * b
+print("The sum of the two numbers is: ", sum)
+print("The product of the two numbers is: ", product)
