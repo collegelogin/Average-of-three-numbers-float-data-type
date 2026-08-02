3e03e7bcdbe62d6ae9969f01e3e352735c990269
collegelogin <collegelogin000@gmail.com>
2026-08-03 11:39:01 +1200

    Input is asked

---
+++ factorial.py
@@ -0,0 +1,11 @@
+#Calculate the factorial number 
+n = int(input("Enter a number to calculate its factorial: "))   
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(f"The factorial of {n} is {factorial(n)}")    
+
+    
